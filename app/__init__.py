c128a5a6a7cb5d06435d72238b79c7351bbf09af
Tulin Dmitry <hijito@yandex.ru>
2021-12-10 15:56:24 +0300

    Add windows compatibility to logs

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 import logging
 import datetime
 
-
 app = Flask(__name__)
 now = datetime.datetime.now()
-logging.basicConfig(filename=f'logs/record_{now.strftime("%d-%m-%Y_%H:%M")}.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+logging.basicConfig(filename=f'logs/record_{now.strftime("%d-%m-%Y_%H%M%S")}.log', level=logging.DEBUG,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 from app import views

37deb0c0a427f7cd7ca0025069a65deec1974ebb
Tulin Dmitry <hijito@yandex.ru>
2021-12-18 17:28:44 +0300

    log system update

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import logging
 import datetime
-
+import os
 
 app = Flask(__name__)
 now = datetime.datetime.now()
-logging.basicConfig(filename=f'logs/record_{now.strftime("%d-%m-%Y_%H%M")}.log', level=logging.DEBUG,
+if not os.path.exists(f'logs/{now.strftime("%d-%m-%Y")}'):
+    os.mkdir(f'logs/{now.strftime("%d-%m-%Y")}')
+
+logging.basicConfig(filename=f'logs/{now.strftime("%d-%m-%Y")}/record_{now.strftime("%d-%m-%Y_%H%M")}.log',
+                    level=logging.DEBUG,
                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 from app import views

7a901a65886ff37d5f782624461066abcf9e8746
Tulin Dmitry <hijito@yandex.ru>
2022-02-23 19:53:31 +0300

    developing of class systen

--- app/data_classes.py
+++ app/data_classes.py
@@ -1,45 +1,24 @@
-from nebula2.gclient.net import ConnectionPool
-from nebula2.Config import Config
-import config
+import communication_with_nebula as cwn
 
 
 class Vertex:
-    Config = Config()
-    Config.max_connection_pool_size = 10
-    # init connection pool
-    connection_pool = ConnectionPool()
-    # if the given servers are ok, return true, else return false
-    ok = connection_pool.init([(config.IP_address, 9669)], Config)
-
-    def __init__(self, name=None, typeOfVertex=None, requirements=None):
+    def __init__(self, name, typeOfVertex = None, requirements=None):
         self.vid = None
         self.session = None
         self.typeOfVertex = typeOfVertex
         self.name = name
         self.requirements = requirements
 
-    def chose_of_space(self):
-        # chose of working space session is still open
-        self.session = self.connection_pool.get_session(config.UserName, config.UserPassword)
-        result = self.session.execute(f'USE {config.WorkSpace}')
-        assert result.is_succeeded(), result.error_msg()
-
     def generate_vid(self):
-        result = self.session.execute(f'LOOKUP ON {self.typeOfVertex}')
-        assert result.is_succeeded(), result.error_msg()
-        result = result.column_values('VertexID')
-        if result:
-            amount = 0
-            for index in result:
-                index = index.as_string()
-                if int(index[len(self.typeOfVertex):]) > amount:
-                    amount = int(index[len(self.typeOfVertex):])
-            self.vid = self.typeOfVertex + str(amount + 1)
-            return
+        if self.typeOfVertex is None:
+            assert 0, 'typeOfVertex is None'
         else:
-            self.vid = self.typeOfVertex + '1'
+            self.vid = self.typeOfVertex + cwn.number_of_entities(cwn.chose_of_space(), self.typeOfVertex)
+
+    def set_vid(self, vid):
+        self.vid = vid
 
 
-a = Vertex(name='YA', typeOfVertex='Compute')
+a = Vertex(name='YA',)
 a.generate_vid()
 print(a.vid)

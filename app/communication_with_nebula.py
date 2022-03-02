f7f3cc42455d9b4001ebe43f71bc78968654039e
Tulin Dmitry <hijito@yandex.ru>
2022-03-02 15:09:33 +0300

    delete old part of code

--- app/communication_with_nebula.py
+++ app/communication_with_nebula.py
@@ -53,56 +53,12 @@ def is_unique_vid(session, vertex_name, vid):
     return True
 
 
-def is_updated(session, vertex_name):
-    result = session.execute(f'LOOKUP ON {vertex_name}')
-    while not result.is_succeeded():
-        print(result.is_succeeded(), result.error_msg(), vertex_name)
-        result = session.execute(f'LOOKUP ON {vertex_name}')
-        session.release()
-        session = chose_of_space()
-    return session
-
-
-def is_already_recorded(session, vertex_name, name_of_key_value, key_value):
-    # return is this already recorded
-    result = session.execute(f'LOOKUP ON {vertex_name} WHERE {vertex_name}.{name_of_key_value} == "{key_value}";')
-    assert result.is_succeeded(), result.error_msg()
-    result = result.column_values('VertexID')
-    if result:
-        result = result[0]
-    return result
-
-
-def create_vertex_if_nox_exist(session, type_of_node, attributes):
-    result = session.execute(f'CREATE TAG IF NOT EXISTS {type_of_node} (name fixed_string(256) NOT NULL)')
-    print(f'CREATE TAG IF NOT EXISTS {type_of_node} (name fixed_string(256) NOT NULL)')
-    assert result.is_succeeded(), result.error_msg()
-    if attributes:
-        for attribute in attributes:
-            result = session.execute(f'ALTER TAG {type_of_node} ADD ({attribute} fixed_string(256) NULL)')
-            if result.is_succeeded() or result.error_msg() != 'Existed!':
-                assert result.is_succeeded(), result.error_msg()
-    result = session.execute(f'CREATE TAG INDEX IF NOT EXISTS Index_{type_of_node} on {type_of_node}(name)')
-    assert result.is_succeeded(), result.error_msg()
-    return
-
-
 def add_in_vertex(session, vertex_name, name_of_key_value, key_value, vid):
     # add into vertex value
     result = session.execute(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
                              f':({key_value});')
     print(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
           f':({key_value});')
-    assert result.is_succeeded(), result.error_msg()
-    return
-
-
-def create_edge_if_nox_exist(session, type_of_link):
-    result = session.execute(f'CREATE EDGE IF NOT EXISTS {type_of_link} ()')
-    # print(f'CREATE TAG IF NOT EXISTS {type_of_link} ()')
-    assert result.is_succeeded(), result.error_msg()
-    result = session.execute(f'CREATE EDGE INDEX IF NOT EXISTS Index{type_of_link} on {type_of_link}()')
-    # print(f'CREATE EDGE INDEX IF NOT EXISTS Index{type_of_link} on {type_of_link}()')
     assert result.is_succeeded(), result.error_msg()
     return
 
@@ -115,116 +71,6 @@ def add_edge(session, edge_name, edge_params, source_vertex, destination_vertex,
     assert result.is_succeeded(), result.error_msg()
     return
 
-
-def add_server(cpu, ram, mem, owner, date):
-    session = chose_of_space()
-    # search for an existing user
-    is_recorded = is_already_recorded(session, config.NameOfUserVertex, config.NameOfKeyValueUser, owner)
-    if not is_recorded:
-        source_vertex = number_of_entities(session, config.NameOfUserVertex)
-        owner = '"' + owner + '"'
-        source_vertex = f'"{config.NameOfUserVertex}{source_vertex}"'
-        add_in_vertex(session, config.NameOfUserVertex, config.NameOfKeyValueUser, owner, source_vertex)
-    else:
-        source_vertex = is_recorded
-    destination_vertex = number_of_entities(session, config.NameOfServerVertex)
-    destination_vertex = f'"{config.NameOfServerVertex}{destination_vertex}"'
-    add_in_vertex(session, config.NameOfServerVertex, config.NameOfKeyValueServer, f'{cpu},{ram},{mem}',
-                  destination_vertex)
-    date = list(date)
-    date[10] = 'T'
-    date = "".join(date)
-    date = f'datetime("{date}")'
-    add_edge(session, config.NameOfOwnerEdge, config.NameOfKeyValueOwner, source_vertex, destination_vertex, date)
-
-    session.release()
-
-
-def find_server(cpu, ram, mem, owner):
-    # return list of this servers with owner
-    session = chose_of_space()
-    owner = is_already_recorded(session, config.NameOfUserVertex, config.NameOfKeyValueUser, owner)
-    if not owner:
-        return 0
-    assert owner, "such owner not existed"
-    result = session.execute(f'GO FROM {owner} over owner'
-                             f' WHERE properties($$).cpu == {cpu} AND properties($$).ram == {ram} AND'
-                             f' properties($$).mem == {mem} '
-                             'YIELD properties($^).name as name, properties($$).cpu as cpu,'
-                             ' properties($$).ram as ram, properties($$).mem as mem,'
-                             ' properties(edge).date_of_creation as date_of_creation')
-
-    assert result.is_succeeded(), result.error_msg()
-    answer = []
-    for key in result.keys():
-
-        tmp_answer = []
-        if not result.column_values(key):
-            return 0
-        for value in result.column_values(key):
-            if value.is_int():
-                tmp_answer += [{
-                    f'{key}': value.as_int()}]
-            elif value.is_string():
-                tmp_answer += [{
-                    f'{key}': value.as_string()}]
-            elif value.is_datetime():
-                tmp_answer += [{
-                    'year': value.as_datetime().get_year(),
-                    'month': value.as_datetime().get_month(),
-                    'day': value.as_datetime().get_day(),
-                    'hour': value.as_datetime().get_hour(),
-                    'minute': value.as_datetime().get_minute(),
-                    'sec': value.as_datetime().get_sec(),
-                    'microsecond': value.as_datetime().get_microsec()}]
-        answer += [tmp_answer]
-
-    session.release()
-    return answer
-
-
-def name_to_index(rename, data):
-    for node in data:
-        node[0] = rename[node[0]]
-        if len(node) > 2:
-            # print(node)
-            if type(node[2]) == list and node[2][0]:
-                for link in node[2]:
-                    # print(link)
-                    link[1] = rename[link[1]]
-    return data
-
-
-def get_attributes_name(data):
-    result = []
-    if data[0]:
-        for frame in data:
-            result += [frame[0]]
-    return result
-
-
-def get_attributes(data):
-    result = []
-    if data[0]:
-        for frame in data:
-            result += [frame[1]]
-    return result
-
-
-def form_name_key_value(data):
-    res = ''
-    for name in data:
-        res += name + ', '
-    res = res[:-2]
-    return res
-
-
-def form_key_value(data):
-    result = ''
-    for key in data:
-        result += '"' + str(key) + '"' + ', '
-    result = result[:-2]
-    return result
 
 
 def yaml_deploy(cluster_vertex: data_classes.ClusterName):

e5ef6c9c8fa242fa6f7dfba046ca81bacc98c4f3
DYDKA4 <hijito@yandex.ru>
2022-06-22 16:19:45 +0300

    AttributeDefinition.py Done

--- parser/linker/tosca_v_1_3/definitions/AttributeDefinition.py
+++ parser/linker/tosca_v_1_3/definitions/AttributeDefinition.py
@@ -1,64 +1,12 @@
-# attributes:
-#   <attribute_name>:
-#     type: # <attribute_type> Required
-#     description: <attribute_description>
-#     default: <default_value>
-#     status: <status_value>
-#     key_schema : <key_schema_definition>
-#     entry_schema: <entry_schema_definition>
 from werkzeug.exceptions import abort
 
-from parser.parser.tosca_v_1_3.definitions.DescriptionDefinition import description_parser
-from parser.parser.tosca_v_1_3.definitions.SchemaDefinition import SchemaDefinition, schema_definition_parser
+from parser.linker.LinkByName import link_by_type_name
+from parser.parser.tosca_v_1_3.definitions.AttributeDefinition import AttributeDefinition
+from parser.parser.tosca_v_1_3.definitions.ServiceTemplateDefinition import ServiceTemplateDefinition
 
 
-class AttributeDefinition:
-    def __init__(self, name: str):
-        self.vid = None
-        self.vertex_type_system = 'AttributeDefinition'
-        self.name = name
-        self.type = None
-        self.description = None
-        self.default = None
-        self.status = None
-        self.entry_schema = None
-        self.key_schema = None
-
-    def set_type(self, attribute_type: str):
-        self.type = attribute_type
-
-    def set_description(self, description: str):
-        self.description = description
-
-    def set_default(self, default: str):
-        self.default = default
-
-    def set_status(self, status: str):
-        self.status = status
-
-    def set_key_schema(self, key_schema: SchemaDefinition):
-        self.key_schema = key_schema
-
-    def set_entry_schema(self, entry_schema: SchemaDefinition):
-        self.entry_schema = entry_schema
-
-
-def attribute_definition_parser(name: str, data: dict) -> AttributeDefinition:
-    attribute = AttributeDefinition(name)
-    if data.get('type'):
-        attribute.set_type(data.get('type'))
-    else:
+def link_attribute_definition(service_template: ServiceTemplateDefinition, attribute: AttributeDefinition) -> None:
+    if type(attribute.type) == str:
+        link_by_type_name(service_template.artifact_types, attribute, 'type',)
+    if str in {type(attribute.type)}:
         abort(400)
-    if data.get('description'):
-        if data.get('description'):
-            description = description_parser(data)
-            attribute.set_description(description)
-    if data.get('default'):  # todo Data type problem?
-        attribute.set_default(data.get('default'))
-    if data.get('status'):
-        attribute.set_status(data.get('status'))
-    if data.get('key_schema'):
-        attribute.set_key_schema(schema_definition_parser(data.get('key_schema')))
-    if data.get('entry_schema'):
-        attribute.set_entry_schema(schema_definition_parser(data.get('entry_schema')))
-    return attribute

705aad77acb01f95e2a8b1adb4fca43d4752e608
Tulin Dmitry <hijito@yandex.ru>
2022-06-28 12:51:09 +0300

    ConditionClauseDefinition.py Fix

--- parser/parser/tosca_v_1_3/definitions/ConditionClauseDefinition.py
+++ parser/parser/tosca_v_1_3/definitions/ConditionClauseDefinition.py
@@ -17,12 +17,6 @@ class ConditionClauseDefinition:
     def __init__(self, type_condition: str):
         self.vid = None
         self.type = type_condition
-        # self.operands = {
-        #     'not': [],
-        #     'or': [],
-        #     'and': [],
-        #     'assert': []
-        # }
         self.condition_not = []
         self.condition_or = []
         self.condition_and = []
@@ -30,27 +24,7 @@ class ConditionClauseDefinition:
         self.vertex_type_system = 'ConditionClauseDefinition'
 
 
-# def sub_parser(condition_clause, condition, operation):
-#     operand = ConditionClauseDefinition(operation)
-#     for sub_condition in condition_clause.get(operation):
-#         sub_condition_structure = None
-#         for sub_condition_key, sub_condition_value in sub_condition.items():
-#             if sub_condition_key not in {'and', 'or', 'not'}:
-#                 assert_dict = {sub_condition_key: []}
-#                 for constraint_clauses in sub_condition_value:
-#                     assert_dict[sub_condition_key]. \
-#                         append(constraint_clause_parser(constraint_clauses))
-#                     operand.operands['assert'].append(assert_dict)
-#             else:
-#                 operand_2 = condition_clause_definition_parser(sub_condition_key,
-#                                                                sub_condition)
-#                 sub_condition_structure = ConditionClauseDefinition(operation)
-#                 sub_condition_structure.operands[sub_condition_key].append(operand_2)
-#         if sub_condition_structure:
-#             condition.operands[operation].append(sub_condition_structure)
-#     if operand.operands['assert']:
-#         condition.operands[operation].append(operand)
-#     pass
+
 
 
 def condition_clause_definition_parser(type_condition: str, data: dict) -> ConditionClauseDefinition:

58d156c98bedbe02282b7678ff95d33d1914fc2f
Tulin Dmitry <hijito@yandex.ru>
2022-06-29 14:42:31 +0300

    PropertyAssignment.py Done

---
+++ nebula_communication/template_builder/assignment/PropertyAssignment.py
@@ -0,0 +1,16 @@
+from nebula_communication.nebula_functions import fetch_vertex
+
+
+def construct_property_assignment(list_of_vid) -> dict:
+    result = {}
+
+    for vid in list_of_vid:
+        vertex_value = fetch_vertex(vid, 'PropertyAssignment')
+        vertex_value = vertex_value.as_map()
+        value = vertex_value['values'].as_string()
+        if value.isnumeric():
+            value: int = int(value)
+        elif value.replace('.', '', 1).isdigit():
+            value: float = float(value)
+        result[vertex_value['name'].as_string()] = value
+    return result

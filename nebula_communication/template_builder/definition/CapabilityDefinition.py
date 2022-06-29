c0dd825e93126b048aacc1259cf2b6219b4a560c
Tulin Dmitry <hijito@yandex.ru>
2022-06-29 16:11:02 +0300

    CapabilityDefinition.py Done NodeTypes.py Done

--- nebula_communication/template_builder/definition/CapabilityDefinition.py
+++ nebula_communication/template_builder/definition/CapabilityDefinition.py
@@ -3,30 +3,29 @@ from werkzeug.exceptions import abort
 from nebula_communication.nebula_functions import fetch_vertex, find_destination
 from nebula_communication.template_builder.definition.AttributeDefinition import construct_attribute_definition
 from nebula_communication.template_builder.definition.ProperyDefinition import construct_property_definition
-from parser.parser.tosca_v_1_3.types.CapabilityType import CapabilityType
+from parser.parser.tosca_v_1_3.definitions.CapabilityDefinition import CapabilityDefinition
 
 
-def construct_capability_type(list_of_vid) -> dict:
+def construct_capability_definition(list_of_vid) -> dict:
     result = {}
-    capability_type = CapabilityType('name').__dict__
+    capability_definition = CapabilityDefinition('name').__dict__
 
     for vid in list_of_vid:
-        vertex_value = fetch_vertex(vid, 'CapabilityType')
+        vertex_value = fetch_vertex(vid, 'CapabilityDefinition')
         vertex_value = vertex_value.as_map()
         tmp_result = {}
         vertex_keys = vertex_value.keys()
         for vertex_key in vertex_keys:
             if not vertex_value[vertex_key].is_null() and vertex_key not in {'vertex_type_system', 'name'}:
                 tmp_result[vertex_key] = vertex_value[vertex_key].as_string()
-        edges = set(capability_type.keys()) - set(vertex_keys) - {'vid'}
+        edges = set(capability_definition.keys()) - set(vertex_keys) - {'vid'}
         for edge in edges:
             destination = find_destination(vid, edge)
-            if edge == 'derived_from':
-                if destination:
-                    derived_from = fetch_vertex(destination[0], 'CapabilityType')
-                    derived_from = derived_from.as_map()
-                    derived_from = derived_from['name'].as_string()
-                    tmp_result['derived_from'] = derived_from
+            if edge == 'type':
+                capability_type = fetch_vertex(destination[0], 'CapabilityType')
+                capability_type = capability_type.as_map()
+                capability_type = capability_type['name'].as_string()
+                tmp_result['type'] = capability_type
             elif edge == 'properties':
                 tmp_result['properties'] = construct_property_definition(destination)
             elif edge == 'attributes':
@@ -38,6 +37,22 @@ def construct_capability_type(list_of_vid) -> dict:
                     data = data.as_map()
                     valid_source_types.append(data['name'].as_string())
                 tmp_result['valid_source_types'] = valid_source_types
+            elif edge == 'occurrences':
+                if destination:
+                    relationship = fetch_vertex(destination[0], 'Occurrences')
+                    relationship = relationship.as_map()
+                    minimum: str = relationship['minimum'].as_string()
+                    maximum: str = relationship['maximum'].as_string()
+                    if minimum.isnumeric():
+                        minimum: int = int(minimum)
+                    elif minimum.replace('.', '', 1).isdigit():
+                        minimum: float = float(minimum)
+
+                    if maximum.isnumeric():
+                        maximum: int = int(maximum)
+                    elif maximum.replace('.', '', 1).isdigit():
+                        maximum: float = float(maximum)
+                    tmp_result['occurrences'] = [minimum, maximum]
             else:
                 abort(500)
         result[vertex_value['name'].as_string()] = tmp_result
--- nebula_communication/template_builder/type/NodeTypes.py
+++ nebula_communication/template_builder/type/NodeTypes.py
@@ -1,32 +1,34 @@
 from werkzeug.exceptions import abort
 
 from nebula_communication.nebula_functions import fetch_vertex, find_destination
+from nebula_communication.template_builder.definition.ArtifactDefinition import construct_artifact_definition
 from nebula_communication.template_builder.definition.AttributeDefinition import construct_attribute_definition
+from nebula_communication.template_builder.definition.CapabilityDefinition import construct_capability_definition
 from nebula_communication.template_builder.definition.InterfaceDefinition import construct_interface_definition
 from nebula_communication.template_builder.definition.MetadataDefinition import construct_metadata_definition
 from nebula_communication.template_builder.definition.ProperyDefinition import construct_property_definition
+from nebula_communication.template_builder.definition.RequirementDefinition import construct_requirement_definition
+from parser.parser.tosca_v_1_3.types.NodeType import NodeType
 
-from parser.parser.tosca_v_1_3.types.RelationshipType import RelationshipType
 
-
-def construct_relationship_type(list_of_vid) -> dict:
+def construct_node_type(list_of_vid) -> dict:
     result = {}
-    relationship_type = RelationshipType('name').__dict__
+    node_type = NodeType('name').__dict__
 
     for vid in list_of_vid:
-        vertex_value = fetch_vertex(vid, 'RelationshipType')
+        vertex_value = fetch_vertex(vid, 'NodeType')
         vertex_value = vertex_value.as_map()
         tmp_result = {}
         vertex_keys = vertex_value.keys()
         for vertex_key in vertex_keys:
             if not vertex_value[vertex_key].is_null() and vertex_key not in {'vertex_type_system', 'name'}:
                 tmp_result[vertex_key] = vertex_value[vertex_key].as_string()
-        edges = set(relationship_type.keys()) - set(vertex_keys) - {'vid'}
+        edges = set(node_type.keys()) - set(vertex_keys) - {'vid'}
         for edge in edges:
             destination = find_destination(vid, edge)
             if edge == 'derived_from':
                 if destination:
-                    derived_from = fetch_vertex(destination[0], 'RelationshipType')
+                    derived_from = fetch_vertex(destination[0], 'NodeType')
                     derived_from = derived_from.as_map()
                     derived_from = derived_from['name'].as_string()
                     tmp_result['derived_from'] = derived_from
@@ -36,16 +38,14 @@ def construct_relationship_type(list_of_vid) -> dict:
                 tmp_result['properties'] = construct_property_definition(destination)
             elif edge == 'attributes':
                 tmp_result['attributes'] = construct_attribute_definition(destination)
+            elif edge == 'requirements':
+                tmp_result['requirements'] = construct_requirement_definition(destination)
             elif edge == 'interfaces':
                 tmp_result['interfaces'] = construct_interface_definition(destination)
-            elif edge == 'valid_target_types':
-                valid_target_types = []
-                for valid_target in destination:
-                    valid_target = fetch_vertex(valid_target, 'CapabilityType')
-                    valid_target = valid_target.as_map()
-                    valid_target = valid_target['name'].as_string()
-                    valid_target_types.append(valid_target)
-                tmp_result['valid_target_types'] = valid_target_types
+            elif edge == 'capabilities':
+                tmp_result['capabilities'] = construct_capability_definition(destination)
+            elif edge == 'artifacts':
+                tmp_result['artifacts'] = construct_artifact_definition(destination)
             else:
                 print(edge)
                 abort(500)

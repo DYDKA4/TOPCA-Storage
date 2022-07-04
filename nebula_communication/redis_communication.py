16798c34ff7cf8299a4bfeef4013f381be47af09
DYDKA4 <hijito@yandex.ru>
2022-07-04 17:29:22 +0300

    del by cluster_name, delete by vid

--- nebula_communication/redis_communication.py
+++ nebula_communication/redis_communication.py
@@ -10,3 +10,13 @@ def add_vid(vid, cluster_name):
         return True
     r.set(vid, cluster_name)
     return False
+
+
+def del_all_by_service_template_name(name):
+    for it in r.scan_iter('*'):
+        if r.get(it) == name:
+            del_by_vid(it)
+
+
+def del_by_vid(vid):
+    r.delete(vid)

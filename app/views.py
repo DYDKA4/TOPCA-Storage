926cfc0b677838ca09bda9f663eb60372392547a
DYDKA4 <hijito@yandex.ru>
2022-07-06 17:04:58 +0300

    Delete all into Flask

--- app/views.py
+++ app/views.py
@@ -5,14 +5,14 @@ import yaml
 from app import constructor_yaml
 from app import find as find_method
 from nebula_communication.deploy import deploy
-from nebula_communication.redis_communication import add_vid
+from nebula_communication.redis_communication import add_vid, delete_all
 from parser.linker.tosca_v_1_3.main_linker import main_linker
 from parser.parser.tosca_v_1_3.definitions.ServiceTemplateDefinition import service_template_definition_parser
 
 
 @app.route('/yaml-template/', methods=['POST', 'PUT', 'GET'])
 @app.route('/yaml-template/<path:varargs>', methods=['PATCH'])
-# curl -F file=@jamlExamples/SBS.yaml 'http://127.0.0.1:5000/yaml-template/?cluster_name=cluster'
+# curl -X POST -F file=@jamlExamples/service_template.yaml  http://127.0.0.1:5000/yaml-template/?cluster_name=cluster
 def yaml_add(varargs=None):
     cluster_name = request.args.get('cluster_name')
     if request.method in ['POST', 'PUT']:
@@ -217,22 +217,10 @@ def yaml_add(varargs=None):
 
 
 @app.route('/yaml-delete', methods=['PATCH'])
-# curl -X PATCH -F file=@jamlExamples/SBS.yaml http://127.0.0.1:5000/yaml-patch?
+# curl -X PATCH http://127.0.0.1:5000/yaml-delete
 def yaml_patch():
-    session = communication_with_nebula.chose_of_space()
-    tags = communication_with_nebula.get_all_tags(session)
-    for tag in tags:
-
-        tag = tag.as_string()
-        vertexes = communication_with_nebula.get_all_vertex(session, tag)
-        print(vertexes)
-        for vertex in vertexes:
-            vertex = vertex.as_string()
-            communication_with_nebula.delete_vertex(session, f'"{vertex}"')
-    session.release()
-    return '''
-            400 Bad Request 
-            '''
+    delete_all()
+    return "200 OK"
 
 
 @app.route('/find/<path:varargs>', methods=['GET'])

10f62fe6cb4f7573e5efb97e3f38cc456f8ccd32
DYDKA4 <hijito@yandex.ru>
2022-07-28 14:31:30 +0300

    AttributeDefinitionUpdater.py Getter

--- nebula_communication/update_template/Definition/AttributeDefinitionUpdater.py
+++ nebula_communication/update_template/Definition/AttributeDefinitionUpdater.py
@@ -3,13 +3,14 @@ from werkzeug.exceptions import abort
 from nebula_communication.generate_uuid import generate_uuid
 from nebula_communication.nebula_functions import find_destination, fetch_vertex, update_vertex, delete_edge, add_edge, \
     add_in_vertex, delete_vertex
+from nebula_communication.update_template.Assignment.RequirementAssignment import form_result, return_all
 from nebula_communication.update_template.Definition.SchemaDefinitionUpdate import update_schema_definition, \
-    add_schema_definition
+    add_schema_definition, get_schema_definition
+from nebula_communication.update_template.Other.ConstraintClauseUpdater import get_constraint_clause
 from parser.parser.tosca_v_1_3.definitions.AttributeDefinition import AttributeDefinition
 
 
-def update_attribute_definition(service_template_vid, father_node_vid, value, value_name, varargs: list, type_update,
-                                cluster_name):
+def start_attribute_definition(father_node_vid, varargs):
     if len(varargs) < 2:
         abort(400)
     destination = find_destination(father_node_vid, varargs[0])
@@ -24,6 +25,12 @@ def update_attribute_definition(service_template_vid, father_node_vid, value, va
             break
     if property_vid_to_update is None:
         abort(400)
+    return property_vid_to_update
+
+
+def update_attribute_definition(service_template_vid, father_node_vid, value, value_name, varargs: list, type_update,
+                                cluster_name):
+    property_vid_to_update = start_attribute_definition(father_node_vid, varargs)
     if len(varargs) == 2:
         if type_update == 'delete':
             delete_vertex('"' + property_vid_to_update.as_string() + '"')
@@ -53,12 +60,10 @@ def update_attribute_definition(service_template_vid, father_node_vid, value, va
             abort(501)
     elif varargs[2] == 'key_schema':
         if not add_schema_definition(type_update, varargs[2:], cluster_name, property_vid_to_update, varargs[2]):
-
             update_schema_definition(service_template_vid, property_vid_to_update, value, value_name, varargs[2:],
                                      type_update, cluster_name)
     elif varargs[2] == 'entry_schema':
         if not add_schema_definition(type_update, varargs[2:], cluster_name, property_vid_to_update, varargs[2]):
-
             update_schema_definition(service_template_vid, property_vid_to_update, value, value_name, varargs[2:],
                                      type_update, cluster_name)
     else:
@@ -74,3 +79,37 @@ def add_attribute_definition(type_update, varargs, cluster_name, parent_vid, edg
         add_edge(edge_name, '', parent_vid, import_definition.vid, '')
         return True
     return False
+
+
+def get_attribute_definition(father_node_vid, value, value_name, varargs: list):
+    attribute_vid_to_update = start_attribute_definition(father_node_vid, varargs)
+    if len(varargs) == 2:
+        vertex_value = fetch_vertex(attribute_vid_to_update, 'AttributeDefinition')
+        vertex_value = vertex_value.as_map()
+        if value_name == 'type':
+            return form_result(attribute_vid_to_update, value_name)
+        elif value_name in vertex_value.keys():
+            if value == vertex_value.get(value_name).as_string():
+                return attribute_vid_to_update.as_string()
+        else:
+            abort(501)
+    elif varargs[2] == 'constraints':
+        destination = find_destination(attribute_vid_to_update, value_name)
+        result, flag = return_all(value, value_name, destination)
+        if flag:
+            return result
+        return get_constraint_clause(father_node_vid, value, value_name, varargs[2:])
+    elif varargs[2] == 'key_schema':
+        destination = find_destination(attribute_vid_to_update, value_name)
+        result, flag = return_all(value, value_name, destination)
+        if flag:
+            return result
+        return get_schema_definition(father_node_vid, value, value_name, varargs[2:])
+    elif varargs[2] == 'entry_schema':
+        destination = find_destination(attribute_vid_to_update, value_name)
+        result, flag = return_all(value, value_name, destination)
+        if flag:
+            return result
+        return get_schema_definition(father_node_vid, value, value_name, varargs[2:])
+    else:
+        abort(400)

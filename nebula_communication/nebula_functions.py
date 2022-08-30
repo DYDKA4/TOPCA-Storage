2e37f1e3e8a63c0d3462776c339ae3553ce55560
DYDKA4 <hijito@yandex.ru>
2022-08-30 14:12:43 +0300

    Time to load upload optimize

--- nebula_communication/nebula_functions.py
+++ nebula_communication/nebula_functions.py
@@ -1,15 +1,15 @@
 import logging
 
 import config
-from nebula_communication import connection_pool, r
+from nebula_communication import connection_pool, r, session
 from nebula_communication.redis_communication import del_by_vid, get_all_vid_from_cluster
 
 
-def start_session():
-    session = connection_pool.get_session('Administator', 'password')
-    result = session.execute(f'USE {config.WorkSpace}')
-    assert result.is_succeeded(), result.error_msg()
-    return session
+# def start_session():
+#     session = connection_pool.get_session('Administator', 'password')
+#     result = session.execute(f'USE {config.WorkSpace}')
+#     assert result.is_succeeded(), result.error_msg()
+#     return session
 
 
 def number_of_entities(session, vertex_name):
@@ -31,14 +31,11 @@ def number_of_entities(session, vertex_name):
 
 def vid_getter(vertex_type):
     vertex_type = vertex_type
-    session = start_session()
     vid = '"' + vertex_type + str(number_of_entities(session, vertex_type)) + '"'
-    session.release()
     return vid
 
 
 def add_in_vertex(vertex_name, name_of_key_value, key_value, vid):
-    session = start_session()
     result = session.execute(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
                              f':({key_value});')
     logging.info(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
@@ -46,12 +43,10 @@ def add_in_vertex(vertex_name, name_of_key_value, key_value, vid):
     # print(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
     #       f':({key_value});')
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return
 
 
 def add_edge(edge_name, edge_params, source_vertex, destination_vertex, data):
-    session = start_session()
     result = session.execute(f'INSERT EDGE {edge_name}({edge_params})'
                              f' VALUE {source_vertex}->{destination_vertex}:({data})')
     logging.info(f'INSERT EDGE {edge_name}({edge_params})'
@@ -59,12 +54,10 @@ def add_edge(edge_name, edge_params, source_vertex, destination_vertex, data):
     # print(f'INSERT EDGE {edge_name}({edge_params})'
     #       f' VALUE {source_vertex}->{destination_vertex}:({interface})')
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return
 
 
 def fetch_vertex(vid, vertex_name):
-    session = start_session()
     result = session.execute(f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)')
     logging.info(f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)')
     assert result.is_succeeded(), result.error_msg()
@@ -74,7 +67,6 @@ def fetch_vertex(vid, vertex_name):
 
 
 def fetch_edge(source_vid, destination_vid, edge_name):
-    session = start_session()
     result = session.execute(f'FETCH PROP ON {edge_name} {source_vid} -> {destination_vid} '
                              f'YIELD properties(edge)')
     logging.info(f'FETCH PROP ON {edge_name} {source_vid} -> {destination_vid} '
@@ -86,7 +78,6 @@ def fetch_edge(source_vid, destination_vid, edge_name):
 
 
 def find_destination(vid, edge_name):
-    session = start_session()
     result = session.execute(f'GO FROM {vid} over {edge_name} YIELD dst(edge) as vid')
     logging.info(f'GO FROM {vid} over {edge_name} YIELD dst(edge) as vid')
     assert result.is_succeeded(), result.error_msg()
@@ -94,71 +85,57 @@ def find_destination(vid, edge_name):
 
 
 def get_all_tags():
-    session = start_session()
     result = session.execute('SHOW TAGS')
     assert result.is_succeeded(), result.error_msg()
     return result.column_values('Name')
 
 
 def delete_vertex(vertex):
-    session = start_session()
     result = session.execute(f'DELETE VERTEX {vertex}')
     logging.info(f'DELETE VERTEX {vertex} ')
     assert result.is_succeeded(), result.error_msg()
     del_by_vid(vertex)
-    session.release()
     return
 
 
 def delete_edge(edge_type, source_vertex, destination_vertex):
-    session = start_session()
     result = session.execute(f'DELETE EDGE {edge_type} {source_vertex} -> {destination_vertex}')
     logging.info(f'DELETE EDGE {edge_type} {source_vertex} -> {destination_vertex}')
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return
 
 
 def update_vertex(vertex_name, vid, value_name, value):
-    session = start_session()
     result = session.execute(f"{'UPDATE'} VERTEX ON {vertex_name} {vid} SET {value_name} = {value}")
     logging.info(f"{'UPDATE'} VERTEX ON {vertex_name} {vid} SET {value_name} = {value}")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return
 
 
 def get_all_vid_from_cluster_by_type(cluster_name, vertex_type_system):
     clusters_vid = get_all_vid_from_cluster(cluster_name)
-    session = start_session()
     result = session.execute(f'LOOKUP ON {vertex_type_system}')
     logging.info(f'LOOKUP ON {vertex_type_system}')
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     result = result.column_values("VertexID")
     vertex_type_system_vid = []
     for vid in result:
         vertex_type_system_vid.append(vid.as_string())
     result = list(set(clusters_vid) & set(vertex_type_system_vid))
-    session.release()
     return result
 
 
 def get_all_vertex(tag):
-    session = start_session()
     result = session.execute(f'LOOKUP ON {tag}')
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result.column_values('VertexID')
 
 
 def go_from_over(vertex_id, edge_name, type_edge=''):
-    session = start_session()
     result = session.execute(f"GO FROM {vertex_id} OVER {edge_name} {type_edge} YIELD dst(edge) as id,"
                              f" src(edge) as id2")
     logging.info(f"GO FROM {vertex_id} OVER {edge_name} {type_edge} YIELD dst(edge) as id, src(edge) as id2")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result
 
 
@@ -169,18 +146,15 @@ def go_from_over_dst_params(vertex_id, edge_name, **params):
             condition = f"WHERE properties($$).{name} == '{value}'"
         else:
             condition += f"and properties($$).{name} == '{value}'"
-    session = start_session()
     result = session.execute(f"GO FROM {vertex_id} OVER {edge_name} {condition} "
                              f"YIELD id($$) as id, properties($$) as props")
     logging.info(f"GO FROM {vertex_id} OVER {edge_name} {condition} "
                  f"YIELD id($$) as id, properties($$) as props")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result
 
 
 def complex_go_from_over_2dst_vertex_param(vertex_id, edge_name_1, edge_name_2, param):
-    session = start_session()
     result = session.execute(f"GO FROM {vertex_id} OVER {edge_name_1} YIELD dst(edge) "
                              f"AS id | GO FROM $-.id OVER {edge_name_2} WHERE properties($$).name == '{param}'"
                              f" YIELD id($^) as id, id($$) as id2")
@@ -188,12 +162,10 @@ def complex_go_from_over_2dst_vertex_param(vertex_id, edge_name_1, edge_name_2, 
                  f"AS id | GO FROM $-.id OVER {edge_name_2} WHERE properties($$).name == '{param}'"
                  f" YIELD id($^) as id, id($$) as id2")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result
 
 
 def complex_go_from_over_1dst_vertex_param(vertex_id, edge_name_1, edge_name_2, edge_param):
-    session = start_session()
     result = session.execute(f"GO FROM {vertex_id} OVER {edge_name_1} WHERE properties($$).name == '{edge_param}'"
                              f"YIELD id($$) AS id  | GO FROM $-.id OVER {edge_name_2}"
                              f" YIELD id($^) as id, id($$) as id2 , properties($$) as props")
@@ -201,7 +173,6 @@ def complex_go_from_over_1dst_vertex_param(vertex_id, edge_name_1, edge_name_2, 
                  f"YIELD id($$) AS id  | GO FROM $-.id OVER {edge_name_2}"
                  f" YIELD id($^) as id, id($$) as id2, properties($$) as props")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result
 
 
@@ -238,11 +209,9 @@ def delete_cluster(cluster_name):
 
 
 def find_path(start_vid, end_vid, type_path=""):
-    session = start_session()
     result = session.execute(f"FIND SHORTEST PATH FROM {start_vid} to {end_vid}  over * {type_path}")
     logging.info(f"FIND SHORTEST PATH FROM {start_vid} to {end_vid}  over * {type_path}")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result
 
 
@@ -253,9 +222,7 @@ def find_vertex_by_properties(vid_type, **params):
             condition = f"WHERE {vid_type}.{name} == '{value}'"
         else:
             condition += f"and {vid_type}.{name} == '{value}'"
-    session = start_session()
     result = session.execute(f"LOOKUP ON {vid_type} {condition} YIELD properties(vertex) as props, id(vertex) as id")
     logging.info(f"LOOKUP ON {vid_type} {condition} ")
     assert result.is_succeeded(), result.error_msg()
-    session.release()
     return result

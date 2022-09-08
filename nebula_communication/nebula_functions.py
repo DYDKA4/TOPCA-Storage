08f9eeccc5e47242472e15feef6056a6425ac86a
DYDKA4 <hijito@yandex.ru>
2022-09-08 18:40:01 +0300

    add NebulaException

--- nebula_communication/nebula_functions.py
+++ nebula_communication/nebula_functions.py
@@ -1,7 +1,6 @@
 import logging
 
-import config
-from nebula_communication import connection_pool, r, session
+from nebula_communication import r, session
 from nebula_communication.redis_communication import del_by_vid, get_all_vid_from_cluster
 
 
@@ -11,12 +10,20 @@ from nebula_communication.redis_communication import del_by_vid, get_all_vid_fro
 #     assert result.is_succeeded(), result.error_msg()
 #     return session
 
+class NebulaException(Exception):
+    def __init__(self, code, query, error_msg):
+        self.error_msg = error_msg
+        self.query = query
+        self.code = code
 
-def number_of_entities(session, vertex_name):
+
+def number_of_entities(vertex_name):
     # return of new index of new entities
     result = session.execute(f'LOOKUP ON {vertex_name}')
     # print(f'LOOKUP ON {vertex_name}')
-    assert result.is_succeeded(), result.error_msg()
+    # assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'LOOKUP ON {vertex_name}', result.error_msg())
     result = result.column_values('VertexID')
     if result:
         amount = 0
@@ -29,12 +36,6 @@ def number_of_entities(session, vertex_name):
         return 1
 
 
-def vid_getter(vertex_type):
-    vertex_type = vertex_type
-    vid = '"' + vertex_type + str(number_of_entities(session, vertex_type)) + '"'
-    return vid
-
-
 def add_in_vertex(vertex_name, name_of_key_value, key_value, vid):
     result = session.execute(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
                              f':({key_value});')
@@ -42,7 +43,10 @@ def add_in_vertex(vertex_name, name_of_key_value, key_value, vid):
                  f':({key_value});')
     # print(f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
     #       f':({key_value});')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'INSERT VERTEX {vertex_name} ({name_of_key_value}) VALUES {vid}'
+                                   f':({key_value});', result.error_msg())
+    # assert result.is_succeeded(), result.error_msg()
     return
 
 
@@ -51,9 +55,10 @@ def add_edge(edge_name, edge_params, source_vertex, destination_vertex, data):
                              f' VALUES {source_vertex}->{destination_vertex}:({data})')
     logging.info(f'INSERT EDGE {edge_name}({edge_params})'
                  f' VALUES {source_vertex}->{destination_vertex}:({data})')
-    # print(f'INSERT EDGE {edge_name}({edge_params})'
-    #                          f' VALUES {source_vertex}->{destination_vertex}:({data})')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'INSERT EDGE {edge_name}({edge_params})'
+                                   f' VALUES {source_vertex}->{destination_vertex}:({data})', result.error_msg())
+    # assert result.is_succeeded(), result.error_msg()
     return
 
 
@@ -61,7 +66,8 @@ def fetch_vertex(vid, vertex_name):
     result = session.execute(f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)')
     # print(f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)')
     logging.info(f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'FETCH PROP ON {vertex_name} {vid} YIELD properties(vertex)', result.error_msg())
 
     if result.column_values('properties(VERTEX)') and result.column_values('properties(VERTEX)')[0].as_map() != {}:
         return result.column_values('properties(VERTEX)')[0]
@@ -73,7 +79,10 @@ def fetch_edge(source_vid, destination_vid, edge_name):
                              f'YIELD properties(edge)')
     logging.info(f'FETCH PROP ON {edge_name} {source_vid} -> {destination_vid} '
                  f'YIELD properties(edge)')
-    assert result.is_succeeded(), result.error_msg()
+    print(result.is_succeeded())
+    if not result.is_succeeded():
+        raise NebulaException(400, f'FETCH PROP ON {edge_name} {source_vid} -> {destination_vid} '
+                                   f'YIELD properties(edge)', result.error_msg())
     if result.column_values('properties(EDGE)'):
         return result.column_values('properties(EDGE)')[0]
     return None
@@ -82,20 +91,23 @@ def fetch_edge(source_vid, destination_vid, edge_name):
 def find_destination(vid, edge_name):
     result = session.execute(f'GO FROM {vid} over {edge_name} YIELD dst(edge) as vid')
     logging.info(f'GO FROM {vid} over {edge_name} YIELD dst(edge) as vid')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'GO FROM {vid} over {edge_name} YIELD dst(edge) as vid', result.error_msg())
     return result.column_values('vid')
 
 
 def get_all_tags():
     result = session.execute('SHOW TAGS')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, 'SHOW TAGS', result.error_msg())
     return result.column_values('Name')
 
 
 def delete_vertex(vertex):
     result = session.execute(f'DELETE VERTEX {vertex} WITH EDGE')
     logging.info(f'DELETE VERTEX {vertex} WITH EDGE')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'DELETE VERTEX {vertex} WITH EDGE', result.error_msg())
     del_by_vid(vertex)
     return
 
@@ -103,14 +115,18 @@ def delete_vertex(vertex):
 def delete_edge(edge_type, source_vertex, destination_vertex):
     result = session.execute(f'DELETE EDGE {edge_type} {source_vertex} -> {destination_vertex}')
     logging.info(f'DELETE EDGE {edge_type} {source_vertex} -> {destination_vertex}')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'DELETE EDGE {edge_type} {source_vertex} -> {destination_vertex}',
+                              result.error_msg())
     return
 
 
 def update_vertex(vertex_name, vid, value_name, value):
     result = session.execute(f"{'UPDATE'} VERTEX ON {vertex_name} {vid} SET {value_name} = {value}")
     logging.info(f"{'UPDATE'} VERTEX ON {vertex_name} {vid} SET {value_name} = {value}")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"{'UPDATE'} VERTEX ON {vertex_name} {vid} SET {value_name} = {value}",
+                              result.error_msg())
     return
 
 
@@ -118,7 +134,8 @@ def get_all_vid_from_cluster_by_type(cluster_name, vertex_type_system):
     clusters_vid = get_all_vid_from_cluster(cluster_name)
     result = session.execute(f'LOOKUP ON {vertex_type_system}')
     logging.info(f'LOOKUP ON {vertex_type_system}')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'LOOKUP ON {vertex_type_system}', result.error_msg())
     result = result.column_values("VertexID")
     vertex_type_system_vid = []
     for vid in result:
@@ -129,7 +146,8 @@ def get_all_vid_from_cluster_by_type(cluster_name, vertex_type_system):
 
 def get_all_vertex(tag):
     result = session.execute(f'LOOKUP ON {tag}')
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f'LOOKUP ON {tag}', result.error_msg())
     return result.column_values('VertexID')
 
 
@@ -137,7 +155,9 @@ def go_from_over(vertex_id, edge_name, type_edge=''):
     result = session.execute(f"GO FROM {vertex_id} OVER {edge_name} {type_edge} YIELD dst(edge) as id,"
                              f" src(edge) as id2")
     logging.info(f"GO FROM {vertex_id} OVER {edge_name} {type_edge} YIELD dst(edge) as id, src(edge) as id2")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"GO FROM {vertex_id} OVER {edge_name} {type_edge} YIELD dst(edge) as id,"
+                                   f" src(edge) as id2", result.error_msg())
     return result
 
 
@@ -152,7 +172,9 @@ def go_from_over_dst_params(vertex_id, edge_name, **params):
                              f"YIELD id($$) as id, properties($$) as props")
     logging.info(f"GO FROM {vertex_id} OVER {edge_name} {condition} "
                  f"YIELD id($$) as id, properties($$) as props")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"GO FROM {vertex_id} OVER {edge_name} {condition} "
+                                   f"YIELD id($$) as id, properties($$) as props", result.error_msg())
     return result
 
 
@@ -163,7 +185,10 @@ def complex_go_from_over_2dst_vertex_param(vertex_id, edge_name_1, edge_name_2, 
     logging.info(f"GO FROM {vertex_id} OVER {edge_name_1} YIELD dst(edge) "
                  f"AS id | GO FROM $-.id OVER {edge_name_2} WHERE properties($$).name == '{param}'"
                  f" YIELD id($^) as id, id($$) as id2")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"GO FROM {vertex_id} OVER {edge_name_1} YIELD dst(edge) "
+                                   f"AS id | GO FROM $-.id OVER {edge_name_2} WHERE properties($$).name == '{param}'"
+                                   f" YIELD id($^) as id, id($$) as id2", result.error_msg())
     return result
 
 
@@ -174,7 +199,10 @@ def complex_go_from_over_1dst_vertex_param(vertex_id, edge_name_1, edge_name_2, 
     logging.info(f"GO FROM {vertex_id} OVER {edge_name_1} WHERE properties($$).name == '{edge_param}'"
                  f"YIELD id($$) AS id  | GO FROM $-.id OVER {edge_name_2}"
                  f" YIELD id($^) as id, id($$) as id2, properties($$) as props")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"GO FROM {vertex_id} OVER {edge_name_1} WHERE properties($$).name == '{edge_param}'"
+                                   f"YIELD id($$) AS id  | GO FROM $-.id OVER {edge_name_2}"
+                                   f" YIELD id($^) as id, id($$) as id2 , properties($$) as props", result.error_msg())
     return result
 
 
@@ -213,7 +241,9 @@ def delete_cluster(cluster_name):
 def find_path(start_vid, end_vid, type_path=""):
     result = session.execute(f"FIND SHORTEST PATH FROM {start_vid} to {end_vid}  over * {type_path} YIELD path as p")
     logging.info(f"FIND SHORTEST PATH FROM {start_vid} to {end_vid}  over * {type_path} YIELD path as p")
-    assert result.is_succeeded(), result.error_msg()
+    if not result.is_succeeded():
+        raise NebulaException(400, f"FIND SHORTEST PATH FROM {start_vid} to {end_vid}  over * {type_path} YIELD path "
+                                   f"as p", result.error_msg())
     return result
 
 
@@ -226,7 +256,10 @@ def find_vertex_by_properties(vid_type, **params):
             condition += f"and {vid_type}.{name} == '{value}'"
     result = session.execute(f"LOOKUP ON {vid_type} {condition} YIELD properties(vertex) as props, id(vertex) as id")
     logging.info(f"LOOKUP ON {vid_type} {condition} YIELD properties(vertex) as props, id(vertex) as id")
-    assert result.is_succeeded(), result.error_msg()
+    # print(f"LOOKUP ON {vid_type} {condition} YIELD properties(vertex) as props, id(vertex) as id")
+    if not result.is_succeeded():
+        raise NebulaException(400, f"LOOKUP ON {vid_type} {condition} YIELD properties(vertex) as props, id(vertex) "
+                                   f"as id", result.error_msg())
     return result
 
 
@@ -255,6 +288,9 @@ def find_all_edges(vid, steps: int, **params):
                              f' YIELD dst(edge) as id, properties($$) as props')
     logging.info(f' GO {steps} STEPS FROM {vid} OVER *  {condition} YIELD dst(edge) as id, properties($$) as props')
     # print(f' GO {steps} STEPS FROM {vid} OVER *  {condition}'
-    #                          f' YIELD dst(edge) as id, properties($$) as props')
-    assert result.is_succeeded(), result.error_msg()
+    #       f' YIELD dst(edge) as id, properties($$) as props')
+    # a = result.is_succeeded()
+    if not result.is_succeeded():
+        raise NebulaException(400, f' GO {steps} STEPS FROM {vid} OVER *  {condition}'
+                                   f' YIELD dst(edge) as id, properties($$) as props', result.error_msg())
     return result

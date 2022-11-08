8531e233fb6c3b2fbfaf110c7cfdb8b339d4c2c2
DYDKA4 <hijito@yandex.ru>
2022-11-08 17:30:42 +0300

    add comments on class, methods, functions.

--- mariadb_parser/type_table/TypeStorage.py
+++ mariadb_parser/type_table/TypeStorage.py
@@ -11,6 +11,7 @@ class DataType:
     DataType(0, 'test_name', {'data': 'value'}, '1.0.0')
     """
 
+    # TODO поиграть с наследованием
     def __init__(self, identifier: int, name: str, data: dict, version: str = ''):
         self.identifier = identifier
         self.name: str = name
@@ -26,7 +27,8 @@ class DataType:
 class TypeStorage:
     """
     TypeStorage class parse part of yaml file with type definition and prepare it to submit it into SQL database
-
+    it consists of:
+        data_types where stored dict where keys are name of DataType and value is DataType object
     """
 
     def __init__(self, data: dict):
@@ -38,7 +40,13 @@ class TypeStorage:
                 self.data_type_linage(data_type)
 
     def prepare_data_types(self, data) -> dict[str, DataType]:
+        """
+        This function make first representation of DataTypes
+        :param data:
+        :return dict of str and DataType:
+        """
         data_types = {}
+        # NOTE can be parallelized
         for name, data in data.items():
             derived_from = data.get('derived_from')
             version = data.get('version')
@@ -53,6 +61,7 @@ class TypeStorage:
                 data_type.dependencies = data_type.dependencies.union(entry_schema_dependencies)
             properties: dict = data.get('properties')
             if properties:
+                # NOTE can be parallelized
                 for property_name, property_value in properties.items():
                     data_type.dependencies = \
                         data_type.dependencies.union(self.get_property_dependencies(property_value, property_name))
@@ -62,6 +71,13 @@ class TypeStorage:
         return data_types
 
     def check_schema_in_entity(self, data: dict, result: set[str]) -> set[str]:
+        """
+        This function checks if there is key_schema or entry_schema in this object
+        It returns set of required data_types for  key_schema or entry_schema and their nested schemas
+        :param data: dict
+        :param result: set[str]
+        :return result: set[str]
+        """
         key_schema: dict = data.get('key_schema')
         if key_schema:
             result = result.union(self.get_schema_dependencies(key_schema))
@@ -71,6 +87,12 @@ class TypeStorage:
         return result
 
     def get_property_dependencies(self, data: dict, name: str) -> set[str]:
+        """
+        This function returns all dependencies of property_definition
+        :param data: dict
+        :param name: str
+        :return result: srt[str]
+        """
         data_type: str = data.get('type')
         result = set()
         if data_type is None:
@@ -80,6 +102,11 @@ class TypeStorage:
         return result
 
     def get_schema_dependencies(self, data: dict) -> set[str]:
+        """
+        This function returns all dependencies of schema_definition
+        :param data: dict
+        :return result: set[str]
+        """
         data_type: str = data.get('type')
         result = set()
         if data_type is None:
@@ -89,12 +116,17 @@ class TypeStorage:
         return result
 
     def data_type_linage(self, data_type: DataType) -> None:
+        """
+        This method restores all previous derived from
+        :param data_type: DataType
+        :return: None
+        """
+        # todo доделать
         for derived_from in data_type.derived_from:
             derived_from: str
             derived_from_structure = self.data_types.get(derived_from)
             if derived_from_structure is None:
-                raise Exception('No such data_type in derived_from from yaml file. DataType: '+data_type.name)
-            derived_from_structure
+                raise Exception('No such data_type in derived_from from yaml file. DataType: ' + data_type.name)
             print(derived_from_structure)
 
 

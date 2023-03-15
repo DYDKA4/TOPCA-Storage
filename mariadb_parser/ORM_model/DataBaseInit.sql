71e41281d6b403348fc7ff5688761bd1151db234
DYDKA4 <hijito@yandex.ru>
2023-03-15 17:14:13 +0300

    database init script fix

--- mariadb_parser/ORM_model/DataBaseInit.sql
+++ mariadb_parser/ORM_model/DataBaseInit.sql
@@ -6,7 +6,7 @@ create table alembic_version
 
 create table artifact_storage
 (
-    id   char(36)                  not null
+    id   char(36)                     not null
         primary key,
     name char(255)                    not null,
     data longtext collate utf8mb4_bin not null,
@@ -16,7 +16,7 @@ create table artifact_storage
 
 create table instance_model
 (
-    id          char(36)                  not null
+    id          char(36)                     not null
         primary key,
     description text                         null,
     metadata    longtext collate utf8mb4_bin null,
@@ -26,7 +26,7 @@ create table instance_model
 
 create table type
 (
-    id                        char(36)                                                                                                                             not null
+    id                        char(36)                                                                                                                                not null
         primary key,
     version                   char(32)                                                                                                                                not null,
     type_of_type              enum ('artifact_type', 'data_type', 'capability_type', 'interface_type', 'relationship_type', 'node_type', 'group_type', 'policy_type') not null,
@@ -40,8 +40,8 @@ create table type
 
 create table dependency_types
 (
-    source_id       char(36)                                                   not null,
-    dependency_id   char(36)                                                   not null,
+    source_id       char(36)                                                      not null,
+    dependency_id   char(36)                                                      not null,
     dependency_type enum ('derived_from', 'requirement_dependency', 'dependency') not null,
     primary key (source_id, dependency_type, dependency_id),
     constraint dependency_id
@@ -54,20 +54,21 @@ create table dependency_types
 
 create table node_template
 (
-    id                char(36)                  not null
+    id                char(36)                     not null
         primary key,
     type_name         char(255)                    not null,
-    type_id           char(36)                  null,
+    type_id           char(36)                     null,
     description       text                         null,
     metadata          longtext collate utf8mb4_bin null,
     copy_name         char(255)                    null,
-    copy_id           char(36)                  null,
-    instance_model_id char(36)                  not null,
+    copy_id           char(36)                     null,
+    instance_model_id char(36)                     not null,
     name              char(255)                    not null,
     constraint copy_fk
         foreign key (copy_id) references node_template (id),
     constraint instace_mode_if
-        foreign key (instance_model_id) references instance_model (id),
+        foreign key (instance_model_id) references instance_model (id)
+            on update cascade on delete cascade,
     constraint node_type_fk
         foreign key (type_id) references type (id),
     constraint metadata
@@ -76,78 +77,85 @@ create table node_template
 
 create table capability
 (
-    id      char(36)                  not null
+    id      char(36)                     not null
         primary key,
     value   longtext collate utf8mb4_bin null,
-    node_id char(36)                  not null,
+    node_id char(36)                     not null,
     name    char(255)                    not null,
     constraint node_id
-        foreign key (node_id) references node_template (id),
+        foreign key (node_id) references node_template (id)
+            on update cascade on delete cascade,
     constraint value
         check (json_valid(`value`))
 );
 
 create table node_interface
 (
-    id      char(36)not null
+    id      char(36)  not null
         primary key,
-    name    char(255)  not null,
-    node_id char(36)not null,
+    name    char(255) not null,
+    node_id char(36)  not null,
     constraint node_interface_node_template_null_fk
         foreign key (node_id) references node_template (id)
+            on update cascade on delete cascade
 );
 
 create table node_interface_operation
 (
-    id                char(36)  not null
+    id                char(36)     not null
         primary key,
     name              varchar(255) not null,
-    implementation    varchar(255) not null,
-    node_interface_id char(36)  not null,
+    implementation    varchar(255) null,
+    node_interface_id char(36)     not null,
     constraint node_interface_operation_node_interface_null_fk
         foreign key (node_interface_id) references node_interface (id)
+            on update cascade on delete cascade
 );
 
 create table requirement
 (
-    id         char(36)not null
+    id         char(36)  not null
         primary key,
-    capability char(255)  null,
-    name       char(255)  not null,
-    node_id    char(36)not null,
-    node       char(255)  not null,
-    node_link  char(36)not null,
-    constraint requirement_node_template_null_fk
-        foreign key (node_id) references node_template (id),
-    constraint requirement_node_template_null_fk2
+    capability char(255) null,
+    name       char(255) not null,
+    node_id    char(36)  not null,
+    node       char(255) not null,
+    node_link  char(36)  not null,
+    constraint requirement_node_template_id_fk_2
         foreign key (node_link) references node_template (id)
+            on update cascade on delete cascade,
+    constraint requirement_node_template_null_fk
+        foreign key (node_id) references node_template (id)
+            on update cascade on delete cascade
 );
 
 create table relationship_interface
 (
-    id             char(36)not null
+    id             char(36)  not null
         primary key,
-    name           char(255)  not null,
-    requirement_id char(36)not null,
+    name           char(255) not null,
+    requirement_id char(36)  not null,
     constraint relationship_interface_requirement_fk
         foreign key (requirement_id) references requirement (id)
+            on update cascade on delete cascade
 );
 
 create table relationship_interface_operation
 (
-    id                        char(36)  not null
+    id                        char(36)     not null
         primary key,
     name                      varchar(255) not null,
-    implementation            varchar(255) not null,
-    relationship_interface_id char(36)  not null,
+    implementation            varchar(255) null,
+    relationship_interface_id char(36)     not null,
     constraint relationship_interface_operation_relationship_interface_fk
         foreign key (relationship_interface_id) references relationship_interface (id)
+            on update cascade on delete cascade
 );
 
 create table ts_to_as
 (
-    artifact_storage_id char(36)not null,
-    type_storage_id     char(36)not null,
+    artifact_storage_id char(36) not null,
+    type_storage_id     char(36) not null,
     primary key (artifact_storage_id, type_storage_id),
     constraint foreign_key_name
         foreign key (artifact_storage_id) references artifact_storage (id)
@@ -159,7 +167,7 @@ create table ts_to_as
 
 create table value_storage
 (
-    id    char(36)                  not null
+    id    char(36)                     not null
         primary key,
     value longtext collate utf8mb4_bin null,
     constraint value
@@ -168,33 +176,34 @@ create table value_storage
 
 create table capability_attribute_and_property
 (
-    id               char(36)                    not null
+    id               char(36)                       not null
         primary key,
     name             char(255)                      not null,
-    value_storage_id char(36)                    not null,
-    capability_id    char(36)                    not null,
+    value_storage_id char(36)                       not null,
+    capability_id    char(36)                       not null,
     type             enum ('attribute', 'property') not null,
     constraint capability_attribute_and_property_value_storage_null_fk
         foreign key (value_storage_id) references value_storage (id),
     constraint capability_fk
         foreign key (capability_id) references capability (id)
+            on update cascade on delete cascade
 );
 
 create table instance_model_input_and_output
 (
-    id                char(36)                  not null
+    id                char(36)                     not null
         primary key,
-    instance_model_id char(36)                  not null,
+    instance_model_id char(36)                     not null,
     key_schema        longtext collate utf8mb4_bin null,
     type_name         char(255)                    null,
-    type_id           char(36)                  null,
+    type_id           char(36)                     null,
     description       text                         null,
     required          tinyint(1)                   null,
     `default`         longtext collate utf8mb4_bin null,
     entry_schema      longtext collate utf8mb4_bin null,
     type              enum ('input', 'output')     not null,
     mapping           longtext collate utf8mb4_bin null,
-    value_storage_id  char(36)                  not null,
+    value_storage_id  char(36)                     not null,
     name              char(255)                    not null,
     constraint data_type_fk
         foreign key (type_id) references type (id)
@@ -217,12 +226,12 @@ create table instance_model_input_and_output
 
 create table node_attribute_and_property
 (
-    id               char(36)                    not null
+    id               char(36)                       not null
         primary key,
     name             char(255)                      not null,
     type             enum ('attribute', 'property') not null,
-    value_storage_id char(36)                    not null,
-    node_id          char(36)                    not null,
+    value_storage_id char(36)                       not null,
+    node_id          char(36)                       not null,
     constraint Node_fk
         foreign key (node_id) references node_template (id)
             on update cascade on delete cascade,
@@ -233,26 +242,27 @@ create table node_attribute_and_property
 
 create table node_interface_operation_input_output
 (
-    id               char(36)              not null
+    id               char(36)                 not null
         primary key,
     name             char(255)                not null,
-    value_storage_id char(36)              not null,
-    operation_id     char(36)              not null,
+    value_storage_id char(36)                 not null,
+    operation_id     char(36)                 not null,
     type             enum ('input', 'output') not null,
     constraint interface_operation_fk
-        foreign key (operation_id) references node_interface_operation (id),
+        foreign key (operation_id) references node_interface_operation (id)
+            on update cascade on delete cascade,
     constraint node_interface_operation_input_output_value_storage_fk
         foreign key (value_storage_id) references value_storage (id)
 );
 
 create table relationship_attribute_and_property
 (
-    id               char(36)                    not null
+    id               char(36)                       not null
         primary key,
     name             char(255)                      not null,
     type             enum ('attribute', 'property') not null,
-    value_storage_id char(36)                    not null,
-    requirement_id   char(36)                    not null,
+    value_storage_id char(36)                       not null,
+    requirement_id   char(36)                       not null,
     constraint relationship_attribute_and_property_requirement_fk
         foreign key (requirement_id) references requirement (id)
             on update cascade on delete cascade,
@@ -263,14 +273,15 @@ create table relationship_attribute_and_property
 
 create table relationship_interface_operation_input_output
 (
-    id               char(36)              not null
+    id               char(36)                 not null
         primary key,
     name             char(255)                not null,
-    value_storage_id char(36)              not null,
-    operation_id     char(36)              not null,
+    value_storage_id char(36)                 not null,
+    operation_id     char(36)                 not null,
     type             enum ('input', 'output') not null,
     constraint relationship_interface_operation_input_output_operation_fk
-        foreign key (operation_id) references relationship_interface_operation (id),
+        foreign key (operation_id) references relationship_interface_operation (id)
+            on update cascade on delete cascade,
     constraint relationship_interface_operation_input_output_value_storage_fk
         foreign key (value_storage_id) references value_storage (id)
 );

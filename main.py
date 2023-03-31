2178e912c9d995f83845d19453d972f63dc118ba
DYDKA4 <hijito@yandex.ru>
2023-03-31 18:16:26 +0300

    addd one more worker

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.routes import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=4000)
+    uvicorn.run("app.routes:app", host="0.0.0.0", port=4000, workers=2)
